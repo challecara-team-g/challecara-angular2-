66ba5278ceea03ad9330a1ed96275ccb93f16803
yuki0508 <enla.festa.la.vita@icloud.com>
2018-11-02 14:28:36 +0900

    taskplus

--- src/app/taskplus/taskplus.ts
+++ src/app/taskplus/taskplus.ts
@@ -3,6 +3,7 @@ import {MyApp} from '../app';
 @Component({
   selector: 'ons-page[taskplus]',
   template: require('./taskplus.html'),
+  styleUrls:['../src/app/taskplus/taskplus.css']
 })
 export class TaskPlusPage {
   constructor(@Inject(forwardRef(() => MyApp)) private app : MyApp) {
