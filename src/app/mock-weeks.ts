b3a74bdb69567c3034bb948e94353c41ef0234c6
fumiya <challe.fumi@gmail.com>
2018-11-06 13:18:21 +0900

    mock-week追加

---
+++ src/app/mock-weeks.ts
@@ -0,0 +1,11 @@
+import {Weeks} from './week';
+
+export const Weeks : Weeks[] = [
+  {week:'Monday'},
+  {week:'Tuesday'},
+  {week:'Wednesday'},
+  {week:'Thursday'},
+  {week:'Friday'},
+  {week:'Saturday'},
+  {week:'Sunday'}
+];
